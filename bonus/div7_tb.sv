ac037cbf6cddb752c14d037400b7a7264b178728
Martin <martinmcchua@gmail.com>
2021-09-27 20:21:55 -0700

    Quick div7 tb

---
+++ bonus/div7_tb.sv
@@ -0,0 +1,21 @@
+module div7_tb();
+
+    logic clk, rst, y;
+
+    div7_fsm dut(.*);
+
+    initial begin
+        clk = 0;
+        forever begin
+            #1;
+            clk = ~clk;
+        end
+    end
+
+    initial begin
+        rst = 1;
+        #3; 
+        rst = 0;
+    end
+
+endmodule
